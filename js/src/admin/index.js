99218bb60251d75a7dbabca56511a5f952ede4d6
ResofireV2 <stryguardian@gmail.com>
2026-03-25 09:26:24 -0500

    Add files via upload

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,8 @@
 import app from 'flarum/admin/app';
 import MenuControlPage from './components/MenuControlPage';
 
+export { MenuControlPage };
+
 app.initializers.add('resofire-menu-control', () => {
   app.extensionData
     .for('resofire-menu-control')
